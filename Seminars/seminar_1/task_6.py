e3613a4bb8b0f2977bcac1a4b4dee8450bb635e7
Alex <35335834+Evlampyev@users.noreply.github.com>
2023-10-19 21:14:51 +0500

    Seminar_1 update

--- Seminars/seminar_1/task_6.py
+++ Seminars/seminar_1/task_6.py
@@ -1,3 +1,14 @@
-"""Таблица умножения как в детской тетради черев f строку"""
-
-
+"""Таблица умножения как в детской тетради через f строку"""
+step: int = 4
+part_begin: int = 2
+part_end: int = 6
+flag: int = 0
+while flag < 2:
+    for i in range(2, 11):
+        for j in range(part_begin, part_end):
+            print(f"{j:>2} x {i:>2} = {i * j:>2}" + ' ' * step, end="")
+        print()
+    print()
+    flag += 1
+    part_begin += step
+    part_end += step
