fc396e017f89c73f831ca1862e2986a81b057d2c
Alex <Evlampyev.Alex@gmail.com>
2023-11-18 21:17:19 +0500

    continue sem_9

--- Seminars/seminar_9/task_2.py
+++ Seminars/seminar_9/task_2.py
@@ -7,21 +7,31 @@ from random import randint
 from typing import Callable
 
 
-def ugadaika(func) -> Callable:
-    def wrapper(*args, **kwargs):
+def ugadaika_decorator(func) -> Callable:
+    """декоратор"""
+
+    def wrapper(user_guess, user_num):
+        """Обертка"""
         if user_num < 0 or user_num > 100:
             user_num = randint(1, 100)
 
         if user_guess not in range(1, 11):
             user_guess = randint(1, 10)
             print(f"Вы ввели не верное количество попыток, их будет: {user_guess}")
-
+        result = func(user_guess, user_num)
         return result
 
     return wrapper
 
 
-def func_ugadaika(user_guess, user_num):
+@ugadaika_decorator
+def func_ugadaika(user_guess: int, user_num: int) -> str:
+    """
+    Угадываем число за указанное количество попыток
+    :param user_guess: количество попыток
+    :param user_num: загаданное число
+    :return: результат угадывания
+    """
     current_guess = 1
     while current_guess <= user_guess:
         ans = int(input(f"Попытка №{current_guess}. Ваш вариант: "))
@@ -40,5 +50,5 @@ if __name__ == '__main__':
     step = int(input("Введите количество попыток: "))
     number = randint(1, 200)
 
-    my_func = ugadaika(number, step)
-    print(my_func())
+    my_func = func_ugadaika(step, number)
+    print(my_func)
---
+++ Seminars/seminar_9/task_3.py
@@ -0,0 +1,54 @@
+# Напишите декоратор, который сохраняет в json файл параметры декорируемой функции и результат, который она возвращает.
+# При повторном вызове файл должен расширяться, а не перезаписываться.
+# Каждый ключевой параметр сохраните как отдельный ключ json словаря.
+# Для декорирования напишите функцию, которая может принимать как позиционные, так и ключевые аргументы.
+# Имя файла должно совпадать с именем декорируемой функции.
+import json
+import os.path
+from random import randint
+from typing import Callable
+
+
+def decorator_function(func) -> Callable:
+    def wrapper(*args, **kwargs):
+        func_name = func.__name__
+        file_name = func_name + '.json'
+        print(f'{file_name = }')
+        if not os.path.exists(file_name):
+            with open(file_name, 'w', encoding='UTF-8') as f:
+                json.dump({}, f)
+
+        with open(file_name, 'r', encoding='UTF-8') as f:
+            data = dict(json.load(f))
+
+        print(f'{data = }')
+        result = func(*args, **kwargs)
+        data[result] = {'args': args}
+        for key, value in kwargs.items():
+            data[result][key] = value
+        with open(file_name, 'w', encoding='UTF-8') as f:
+            json.dump(data, f)
+
+        return result
+
+    return wrapper
+
+
+@decorator_function
+def function(*args, **kwargs) -> int:
+    """Возведение числа в степень"""
+    number, number_pow = args[0], args[1]
+    return pow(number, number_pow)
+
+
+@decorator_function
+def ather_func():
+    pass
+
+
+if __name__ == '__main__':
+    num = randint(2, 5)
+    num_pow = randint(2, 6)
+    my_dict = {1: 'one', 2: 'two'}
+    my_func = function(num, num_pow, key='my_dict')
+    print(f'{num} ^ {num_pow} = {my_func}')
